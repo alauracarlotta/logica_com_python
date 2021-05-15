308fe60aba458150f1c11c31f5e692e3096765b4
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-15 01:51:12 -0300

    ex001 - first code - show user 'hello world' message / ex002 - colected input from user and show user a welcome message

---
+++ ex002_nome_usuario/ex002.js
@@ -0,0 +1,5 @@
+alert("Bem-Vindo, usuário!");
+
+let nome = prompt("Escreva o seu nome:");
+
+alert(`${nome}, você terá acesso ao sistema a partir de agora!`);
---
+++ ex002_nome_usuario/ex002.py
@@ -0,0 +1,3 @@
+name = input("Digite o seu nome: ")
+
+print("Seja bem vinda, {}!".format(name))
