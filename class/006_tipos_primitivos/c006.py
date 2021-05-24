7324f5165a864a69af8da8b37065ba2baebb3556
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-24 20:54:56 -0300

    c006 - Tipos Primitivos

---
+++ class/006_tipos_primitivos/c006.py
@@ -0,0 +1,10 @@
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+value = number1 + number2
+
+print(f'A soma é {value}!')
+
+# int = inteiro => 7, -4, 0, 9875
+# float = ponto flutuante (decimais) => 4.5, 0.076, -15.223, 7.0
+# bool = lógicos, booleano => (True or False)
+# str = string (letras, palavras) => 'Olá', '', '7.5'
