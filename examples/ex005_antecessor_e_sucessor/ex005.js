f8adcabfea1be80d075522a4d8a8799c62d53adf
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-26 00:50:04 -0300

    ex005 - predecessor and successor of a number with JS

--- examples/ex005_antecessor_e_sucessor/ex005.js
+++ examples/ex005_antecessor_e_sucessor/ex005.js
@@ -2,6 +2,9 @@ function calculate() {
     let userInput = Number(document.querySelector('#input1').value);
     let divAnswer = document.querySelector('.answer');
 
-    divAnswer.innerHTML += `<div class="answer predecessor">O número <strong>${--userInput}</strong> é o antecessor de ${++userInput}!</div>`;
-    divAnswer.innerHTML += `<div class="answer successor">O sucessor de ${userInput} é <strong>${++userInput}</strong>!</div>`;
+    /* divAnswer.innerHTML += `<div class="answer predecessor">O número <strong>${--userInput}</strong> é o antecessor de ${++userInput}!</div>`;
+    divAnswer.innerHTML += `<div class="answer successor">O sucessor de ${userInput} é <strong>${++userInput}</strong>!</div>`; */
+
+    divAnswer.innerHTML += `<div class="answer predecessor">O número <strong>${userInput-1}</strong> é o antecessor de ${userInput}!</div>`;
+    divAnswer.innerHTML += `<div class="answer successor">O sucessor de ${userInput} é <strong>${userInput+1}</strong>!</div>`;
 }
