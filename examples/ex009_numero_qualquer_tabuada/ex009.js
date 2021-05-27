b43cf5a242d90c0fe337a1425dd734c136778767
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-27 17:03:24 -0300

    ex009 - multiplication table with JS

--- examples/ex009_numero_qualquer_tabuada/ex009.js
+++ examples/ex009_numero_qualquer_tabuada/ex009.js
@@ -1,1 +1,23 @@
+function calculate() {
 
+    let inputMultiplicationTable = document.querySelector('#input1').value;
+    let inputMultiplicationTableNumber = Number(inputMultiplicationTable);
+    let divAnswer = document.querySelector('.answer');
+
+    if(inputMultiplicationTable == "") {
+
+        divAnswer.innerHTML = `<div class="answer error">INFORME UM VALOR!</div>`;
+        
+    } else {
+
+        divAnswer.innerHTML = "";
+
+        for (let cont = 1; cont <= 10; cont++){
+
+            divAnswer.innerHTML += `<div class="answer"><strong>${inputMultiplicationTable}</strong> x <strong>${cont}</strong> = <strong>${inputMultiplicationTableNumber*cont}</strong></div>`;
+            
+        }
+
+    }
+
+}
