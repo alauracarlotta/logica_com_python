de0acad2394794828cd384b5711e3b611a3345e2
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-28 01:41:24 -0300

    ex013 - salary with 15 percent increase with Python

--- examples/ex013_salario_funcionario_15_por_cento_aumento/ex013.py
+++ examples/ex013_salario_funcionario_15_por_cento_aumento/ex013.py
@@ -1,1 +1,7 @@
 # Faça um algoritmo que leia o salário e mostre seu novo salário, com 15% de aumento.
+
+salary = float(input('Digite o salário atual: R$ '))
+
+print(f'O seu salário atual é de R$ {salary:.2f} reais!')
+print(f'Com o aumento de 15%, seu salário será de R$ {(salary + (salary*15/100)):.2f} reais!')
+print('PARABÉNS!')
