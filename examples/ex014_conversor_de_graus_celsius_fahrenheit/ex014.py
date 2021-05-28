f1081bd3478e57969534da90bae18af5780bf671
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-28 03:40:15 -0300

    insered ex014 and ex015 in Examples file

---
+++ examples/ex014_conversor_de_graus_celsius_fahrenheit/ex014.py
@@ -0,0 +1,1 @@
+# Faça um programa que solicte ao usuário um valor de graus Celsius e mostre a ele o valor em graus Fahrenheit.
---
+++ examples/ex015_aluguel_carros/ex015.py
@@ -0,0 +1,1 @@
+# Faça um programa que pergunte ao usuário quantos dias o carro foi alugado e quantos kms foram rodados, gerando assim o valor a ser pago pelo aluguel! (dias: 8, km rodados: 720, valor total a pagar R$ 588,00 reais)
