61c68a81390ec65fca2eb363580b72c6407336b5
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-28 03:14:49 -0300

    ex010 corrected

--- examples/ex010_dinheiro_carteira_quantos_dolares/ex010.js
+++ examples/ex010_dinheiro_carteira_quantos_dolares/ex010.js
@@ -13,7 +13,15 @@ function calculate() {
     } else {
 
         divAnswer.innerHTML = "";
-        divAnswer.innerHTML += `<div class="answer true">Com R$${inputMoneyInTheWalletNumber.toFixed(2)} reias você consegue comprar $${(inputMoneyInTheWalletNumber/5.24).toFixed(2)} dólares!</div>`;
-
+        divAnswer.innerHTML += `<div class="answer false">Sendo a cotação, na data de hoje, 27/05/2021: `;
+        divAnswer.innerHTML += `<div class="answer false">$ Dólar R$ 5,24</div>`;
+        divAnswer.innerHTML += `<div class="answer false">€ Euro R$ 6,38</div>`;
+        divAnswer.innerHTML += `<div class="answer false">£ Libra R$ 7,43</div>`;
+        divAnswer.innerHTML += `<div class="answer false">¥ Yen R$ 0,048</div></br>`;
+        divAnswer.innerHTML += `<div class="answer true">Com R$${inputMoneyInTheWalletNumber.toFixed(2)} reias você consegue comprar: </div>`; 
+        divAnswer.innerHTML += `<div class="answer true"><strong>$${(inputMoneyInTheWalletNumber/5.24).toFixed(2)}</strong> dólares!</div>`;
+        divAnswer.innerHTML += `<div class="answer true"><strong>€${(inputMoneyInTheWalletNumber/6.38).toFixed(2)}</strong> euros!</div>`;
+        divAnswer.innerHTML += `<div class="answer true"><strong>£${(inputMoneyInTheWalletNumber/7.43).toFixed(2)}</strong> Libras!</div>`;
+        divAnswer.innerHTML += `<div class="answer true"><strong>¥${(inputMoneyInTheWalletNumber/0.048).toFixed(2)}</strong> Yens!</div>`;
     }
 }
