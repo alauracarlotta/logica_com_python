7007f9cc0f6ac866f2101c8d09e6867552cef473
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-29 01:11:09 -0300

    ex018 - sino, cosine and tangent with JS

--- examples/3_usando_modulos/ex018_angulo_seno_cosseno_tangente/ex018.py
+++ examples/3_usando_modulos/ex018_angulo_seno_cosseno_tangente/ex018.py
@@ -1,8 +1,8 @@
 # Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 
-from math import cos, sin, tan
+from math import cos, sin, tan, radians
 
 angle = int(input('Digite o valor do ângulo a ser calculado seno, cosseno e tangente: '))
-print(f'O seno de {angle}° é = {sin(angle)}')
-print(f'O cosseno de {angle}° é = {cos(angle)}')
-print(f'A tangente de {angle}° é = {tan(angle)}')
+print(f'O seno de {angle}° é = {sin(radians(angle)):.3f}')
+print(f'O cosseno de {angle}° é = {cos(radians(angle)):.3f}')
+print(f'A tangente de {angle}° é = {tan(radians(angle)):.3f}')
