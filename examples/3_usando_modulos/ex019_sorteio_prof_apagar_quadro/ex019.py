5519826d5bbeae7195ec32d6bb66f27b69c8500a
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-31 02:15:05 -0300

    ex019 - python corrected

--- examples/3_usando_modulos/ex019_sorteio_prof_apagar_quadro/ex019.py
+++ examples/3_usando_modulos/ex019_sorteio_prof_apagar_quadro/ex019.py
@@ -1,11 +1,14 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
 
-from random import  random
+from random import choice
 
 name1 = input('Escreva o nome do aluno 1: ')
 name2 = input('Escreva o nome do aluno 2: ')
 name3 = input('Escreva o nome do aluno 3: ')
 name4 = input('Escreva o nome do aluno 4: ')
-drawStudent = (random(name1, name2, name3, name4))
 
-print(f'O aluno a apagar o quadro é: {drawStudent}')
+listNames = [name1, name2, name3, name4]
+
+studentChoiced = choice(listNames)
+
+print(f'O aluno a apagar o quadro é: {studentChoiced}!')
