4096efb084c2cee39fcb68a4fa3a34a010cf6b73
lauracarlotta <carlotta.custodio@gmail.com>
2021-06-01 04:09:20 -0300

    ex024 - City starts with Santo word with Python

--- examples/3_usando_modulos/009_manipulando_texto/ex024_cidade_comeca_com_santo/ex024.py
+++ examples/3_usando_modulos/009_manipulando_texto/ex024_cidade_comeca_com_santo/ex024.py
@@ -1,1 +1,11 @@
 # faça um programa que leia o nome de uma cidade e diga se ela começa ou não com "Santo" 
+
+city = input('Digite o nome de uma cidade: ')
+
+if "Santo" in city and city[0] == "S":
+
+    print(f'Sim, a cidade começa com "Santo". \nA cidade digitada foi {city}!')
+
+else:
+
+    print(f'Não, a cidade digitada foi {city}!')
