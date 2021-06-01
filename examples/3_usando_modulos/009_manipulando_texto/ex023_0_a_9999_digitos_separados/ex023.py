dd1e148c7d13910e4d337819b5c728f29a29b266
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-31 23:33:57 -0300

    c009 - added exercices

---
+++ examples/3_usando_modulos/009_manipulando_texto/ex023_0_a_9999_digitos_separados/ex023.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
+# Ex: 1834
+
+# unidade: 4
+# dezena: 3
+# centena: 8
+# milhar: 1
+
+# Podemos fazer matematicamente falando ou como strings
