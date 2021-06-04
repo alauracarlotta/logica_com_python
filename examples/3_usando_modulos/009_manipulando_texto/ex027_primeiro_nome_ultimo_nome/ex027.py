213acef5e2fa2ae1f9ae795d857458f2071d97ea
lauracarlotta <carlotta.custodio@gmail.com>
2021-06-04 00:20:38 -0300

    ex027 - Complete name, first name and last name with Python

--- examples/3_usando_modulos/009_manipulando_texto/ex027_primeiro_nome_ultimo_nome/ex027.py
+++ examples/3_usando_modulos/009_manipulando_texto/ex027_primeiro_nome_ultimo_nome/ex027.py
@@ -3,3 +3,17 @@
 # Ex: Ana Maria de Souza
 # primeiro = Ana
 # último = Souza
+
+completeName = input("digite o seu nome completo: ")
+
+completeNameSplitted = completeName.title().split()
+
+if completeName.isnumeric():
+
+    print('Informe um nome válido!')
+
+else:
+
+    print(f'O nome completo informado foi {completeName.title()}')
+    print(f'O 1º nome é {completeNameSplitted[0]}.')
+    print(f'O último nome é {completeNameSplitted[-1]}.')
