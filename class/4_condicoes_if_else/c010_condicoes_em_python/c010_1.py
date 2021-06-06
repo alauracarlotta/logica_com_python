7eb8aae1efe9f32a21f19ef4ca6d85c1bfafc6dd
Laura Carlotta <carlotta.custodio@gmail.com>
2021-06-06 02:08:50 -0300

    class 010 - undertanding if and else conditionals with Python

---
+++ class/4_condicoes_if_else/c010_condicoes_em_python/c010_1.py
@@ -0,0 +1,19 @@
+# Meia de notas:
+
+note1 = float(input("Qual foi a primeira nota: "))
+note2 = float(input("Qual foi a segunda nota? "))
+average = (note1 + note2) / 2
+
+if average >= 7.5:
+    print('PARABÉNS! Você passou!')
+
+else:
+    print(f'Com a nota {average} você não passa para o próximo ano!')
+
+print('OBRIGADA POR UTILIZAR O NOSSO PROGRAMA!')
+
+# note1 = float(input("Qual foi a primeira nota: "))
+# note2 = float(input("Qual foi a segunda nota? "))
+# average = (note1 + note2) / 2
+# print('PARABÉNS! Você passou!' if average >= 7.5 else print(f'Com a nota {average} você não passa para o próximo ano!')) 
+# print('OBRIGADA POR UTILIZAR O NOSSO PROGRAMA!')   
