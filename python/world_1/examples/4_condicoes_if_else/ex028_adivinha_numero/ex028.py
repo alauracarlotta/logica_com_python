061f849e3ddcf8610a2657c359b8446599ffff15
Laura Carlotta <carlotta.custodio@gmail.com>
2021-06-06 16:56:41 -0300

    ex028 - Guess a number between 0 and 5 with Python

--- python/world_1/examples/4_condicoes_if_else/ex028_adivinha_numero/ex028.py
+++ python/world_1/examples/4_condicoes_if_else/ex028_adivinha_numero/ex028.py
@@ -1,2 +1,15 @@
 # Escreva um programa que faça o computador "pensar" num numero inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o numero escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu!
+
+from random import randint
+
+userNumber = int(input('Digite um número de 0 a 5: '))
+randomNumber = randint(0, 5)
+
+if userNumber == randomNumber:
+
+    print('Você acertou!')
+
+else:
+
+    print(f'Você errou, o número sorteado foi {randomNumber}.')
