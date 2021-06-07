a2b84c5c5232ea693e97cda860894bab5fdc754a
Laura Carlotta <carlotta.custodio@gmail.com>
2021-06-06 22:24:57 -0300

    c011 - coloring your code

---
+++ python/world_1/class/5_adicionando_cores_no_Python/c011_1.py
@@ -0,0 +1,11 @@
+print("\033[0;30;41mTeste\033[m")
+
+print("\033[4;33;44mTeste\033[m")
+
+print("\033[1;35;43mTeste\033[m")
+
+print("\033[30;42m Teste\033[m")
+
+print("\033[mTeste\033[m")
+
+print("\033[7;30mTeste\033[m")
---
+++ python/world_1/class/5_adicionando_cores_no_Python/c011_3.py
@@ -0,0 +1,9 @@
+name = 'Guanabara'
+colors = {
+    'clean': '\033[m',
+    'blue': '\033[34m',
+    'yellow': '\033[33m',
+    'blackandwhite': '\033[7;30m'
+}
+
+print(f"Olá, muito prazer em te conhecer, {colors['blue']}{name}{colors['clean']}!!!")
