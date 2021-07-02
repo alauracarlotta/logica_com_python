2d0c0c28a2c9231fe4460d8c64b0697aba7aef79
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-07-02 18:32:10 -0300

    ex032 - Leap year with JS

---
+++ python/world_1/examples/4_condicoes_if_else/ex032_ano_bissexto/ex032.js
@@ -0,0 +1,20 @@
+
+function calculate() {
+
+    let inputYear = document.querySelector("#input1").value;
+    let inputYearConveted = Number(inputYear);
+    let divAnswer = document.querySelector(".answer");
+
+    if (inputYear == "" || inputYear < 0 || isNaN(inputYearConveted)) {
+        divAnswer.innerHTML = `<div class="answer error">Informe um valor válido!</div>`;
+
+    } else if (inputYearConveted % 4 == 0 && inputYearConveted % 100 != 0 || inputYearConveted % 400 == 0) {
+
+        divAnswer.innerHTML = `<div class="answer true">O ano ${inputYearConveted} é BISSEXTO!</div>`;
+
+    } else {
+        divAnswer.innerHTML = `<div class="answer error">O ano ${inputYearConveted} NÃO é BISSEXTO!</div>`;
+        divAnswer.innerHTML += `<div class="answer error">Este ano tem 365 dias!</div>`;
+    }
+    
+}
