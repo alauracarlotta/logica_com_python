032a49dc040e75f9280fc15c47c114093c1cef06
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-07-31 18:08:26 -0300

    ex037 - Binary, octal and hexadecimal numbers convert funish with Javascript

--- python/world_2/examples/6_condicoes_(if..elif)/condicoes aninhadas/ex037_binario_octal_hexadecimal/ex037.js
+++ python/world_2/examples/6_condicoes_(if..elif)/condicoes aninhadas/ex037_binario_octal_hexadecimal/ex037.js
@@ -6,48 +6,62 @@
     const buttonSendInfos = document.querySelector( '[data-send-button]' );
     buttonSendInfos.addEventListener( 'click' , function () {
 
-        const inputNumber = Number(document.getElementById( 'input1' ).value);
-    
-        /* if (!validInputs(inputNumber, divHTML, message)) {
-            console.log('test');
-        } */
-
-        let binario = inputNumber.toString(2)
-        console.log(binario);
-
+        let inputNumber = Number(document.getElementById( 'input1' ).value);
+        let inputOptions = Number(document.getElementById( 'input2' ).value);
+        
+        let binary = inputNumber.toString(2)
         let octal = inputNumber.toString(8)
-        console.log(octal);
-
         let hexadecimal = inputNumber.toString(16)
-        console.log(hexadecimal);
+
+        if ( validInputs( inputNumber , inputOptions ) ) {
+            switch(inputOptions) {
+
+                case 1: 
+                    message = `O valor de ${inputNumber} para binário é ${binary}`;
+                    showInfo( message , 'true' );
+                    break;
+
+                case 2: 
+                    message = `O valor de ${inputNumber} para octal é ${octal}`;
+                    showInfo( message , 'true' );
+                    break;
+
+                case 3: 
+                    message = `O valor de ${inputNumber} para hexadecimal é ${hexadecimal}`;
+                    showInfo( message , 'true' );
+                    break;
+
+                case 4: 
+                    message = `O valor de ${inputNumber} para binário é ${binary}, para octal é ${octal} e para hexadecimal é ${hexadecimal}`;
+                    showInfo( message , 'true' );
+                    break;
+
+            }
+        }
+
+        
         
     });
 
-    const validInputs = ( number1 , number2 = 0 , number3 = 0) => {
+    const validInputs = ( number1 , number2 ) => {
 
-    if ( number1 == '' || number2 == '' || number3 == '' ) {
-        message = 'Não pode haver campos vazios!';
-        showInfo( message , 'error' );
-        return true;
-    }
+        if ( number1 == '' || number2 == '') {
+            message = 'Não pode haver campos vazios!';
+            showInfo( message , 'error' );
+            return true;
+        }
 
-    if ( 
-        number1 < 0 || 
-        number2 < 0 || 
-        number3 < 0 || 
-        isNaN( Number ( number1 ) ) || 
-        isNaN( Number ( number2 ) ) || 
-        isNaN( Number ( number3 ) ) ) {
+        if ( isNaN( ( number1 ) ) || isNaN( ( number2 ) ) ) {
             message = 'Informe um valor válido!';
             showInfo( message , 'error' );
             return true;
-    }
+        }
 
-    if ( number3 > 480 ) {
-        message = 'O número máximo de parcelas é 480 parcelas!';
-        showInfo( message , 'error')
-        return true;
-    }
+        if ( number2 < 1 || number2 > 4) {
+            message = 'Digite uma opção válida!';
+            showInfo( message , 'error' );
+            return true;
+        }
 
     return validInputs;
 
