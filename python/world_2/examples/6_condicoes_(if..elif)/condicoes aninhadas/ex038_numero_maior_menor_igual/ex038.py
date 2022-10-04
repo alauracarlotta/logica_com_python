0359c69d5f040a65e2a17741332a7ea0f2630be2
lauracarlotta <carlotta.custodio@gmail.com>
2022-10-03 21:52:14 -0300

    ex038 with Python

--- python/world_2/examples/6_condicoes_(if..elif)/condicoes aninhadas/ex038_numero_maior_menor_igual/ex038.py
+++ python/world_2/examples/6_condicoes_(if..elif)/condicoes aninhadas/ex038_numero_maior_menor_igual/ex038.py
@@ -3,3 +3,10 @@
 # - O primeiro valor é maior
 # - O segundo valor é maior
 # - Não existe valor maior, os dois são iguais
+
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+
+total = number1 + number2
+
+print(total)
